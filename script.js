5ef605b21bfa43cfe14562c308b43b0faf8b29ad
xEGAx1 <egorsamohin228@gmail.com>
2026-05-21 00:11:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const button = document.getElementById("startBtn");
+const button = document.getElementById("exploreBtn");
 
 button.addEventListener("click", () => {
-  alert("Добро пожаловать в Hair Study");
+
+  document.querySelector("#education").scrollIntoView({
+    behavior: "smooth"
+  });
+
+});
+
+
+const cards = document.querySelectorAll(
+  ".about-card, .education-card, .scheme-card"
+);
+
+cards.forEach(card => {
+
+  card.addEventListener("mousemove", (e) => {
+
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    card.style.background =
+      `radial-gradient(circle at ${x}px ${y}px, #222, #000)`;
+
+    card.style.color = "white";
+
+  });
+
+  card.addEventListener("mouseleave", () => {
+
+    card.style.background = "white";
+    card.style.color = "black";
+
+  });
+
 });
